6fac3448a08345ff54ac79416a90e4b9ae998996
Xiangui Wang <Xiangui.Wang@shopee.com>
2023-06-06 11:14:35 +0800

    add leetcode

--- leetcode/src/leetcode1012.cc
+++ leetcode/src/leetcode1012.cc
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
@@ -8,16 +9,17 @@ public:
     int numDupDigitsAtMostN(int n) {
         string s = to_string(n);
 
-        //从i开始到n不包含重复数字
-        int memo[s.length()][1<<10][2];
+        // 从i开始到n不包含重复数字，且[0,i-1] 不是same的数量
+        // mask 状态压缩
+        int memo[s.length()][1<<10];
         //从i开始到n包含重复数字
         memset(memo,-1,sizeof(memo));
-        function<int(int,int,bool)> dfs = [&](int i,int mask,int same){
+        function<int(int,int,bool)> dfs = [&](int i,int mask,bool same){
             if(i == s.length()){
                 return 1;
             }
-            if(memo[i][mask][same] != -1){
-                return memo[i][mask][same];
+            if(!same && memo[i][mask] != -1){
+                return memo[i][mask];
             }
             int res = 0;
             int up = same ? s[i] - '0' : 9;
@@ -26,10 +28,10 @@ public:
                 int m = (mask == 0 && d == 0) ? mask : (mask | (1<<d));
                 res = res + dfs(i+1,m,same && d==up);
             }
-            memo[i][mask][same] = res;
+            if(!same) memo[i][mask]= res;
             return res;
         };
-        return n - dfs(0,0,true) +1;
+        return n - dfs(0,0,true) + 1;
     }
 };
 
