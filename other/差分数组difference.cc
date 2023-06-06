8d325a9faf2afaf81d47f30b2d59fbd47a38de64
Xiangui Wang <Xiangui.Wang@shopee.com>
2023-06-06 15:02:29 +0800

    fix bug

--- other/差分数组difference.cc
+++ other/差分数组difference.cc
@@ -4,44 +4,31 @@
 using namespace std;
 
 /*
-
+    差分数组是一种常用的数据结构，用于高效处理数组的区间增减操作。它可以在 O(1) 的时间内完成区间增减操作，而不需要修改原始数组的每个元素
 */
 
-vector<int> difference(vector<int>& nums,vector<vector<int>>& updates){
+// 原数组:  [1,4,5,6,3]
+// 差分数组: [1,3,1,1,-3]
+// 差分数组的前缀和是: [1,4,5,6,3]
+// 差分数组区间增加一个值，例如在[2，4]区间加上 diff[start] += 3,diff[end+1] -=3
+void difference(){
+    vector<int> nums = {1,4,5,6,4}; //原数组
     int n = nums.size();
-    vector<int> d(n,0);
-    for(int i=0;i<n;i++){
-        d[i] += nums[i];
-        if(i + 1 < n){
-            d[i+1] -= nums[i];
-        }
+    // 由原数组创建diff数组
+    vector<int> diff(n);
+    diff[0] = nums[0];
+    for(int i=1;i<n;i++){
+        diff[i] = nums[i] - nums[i-1];
     }
 
-    // 区间增加一个值
-    for(auto & update : updates){
-        int l = update[0];
-        int r = update[1];
-        int v = update[2];
-        d[l] += v;
-        if(r + 1 < n) d[r+1] -= v;
-    }
+    // 在区间[2,4]中每个数加上3
+    diff[2] += 3;
+    diff[4+1] -= 3;
 
+    // 由差分数组返回原数组
     vector<int> ans(n,0);
-    ans[0] = d[0];
-    for(int i=1;i<nums.size();i++){
-        ans[i] += ans[i-1] + d[i];
+    ans[0] = diff[0];
+    for(int i=1;i<n;i++){
+        ans[i] = diff[i] + diff[i-1];
     }
-    return ans;
-}
-
-
-
-int main(int argc, char const *argv[]){
-    vector<int> nums = {1,2,3,4,5};
-    vector<vector<int>> updates = {{2,3,5}};
-    vector<int> ans = difference(nums,updates);
-    for(int i=0;i<ans.size();i++){
-        std::cout << ans[i] << std::endl;
-    }
-    return 0;
 }
