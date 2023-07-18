dbc84ff0823541852339b3811646563c33874967
wxquare <wxquare@163.com>
2023-07-18 23:47:24 +0800

    add code

--- other/ac_automation.cc
+++ other/ac_automation.cc
@@ -5,16 +5,21 @@
 
 using namespace std;
 
+
+const int inf = 0x3f3f3f3f;
+
 // AC 自动机的节点结构
 struct Node {
     bool isEnd; // 是否是一个敏感词的结尾节点
+    int minLen;  // 到节点恰好有终点的最短字符串的长度
     unordered_map<char, Node*> children; // 子节点
     Node* fail; // 失败指针，用于构建自动机
-    Node() : isEnd(false), fail(nullptr) {}
+    Node() : isEnd(false),minLen(inf),fail(nullptr) {}
+    string data;
 };
 
 // 构建 AC 自动机
-void buildACAutomaton(Node* root, const vector<string>& keywords) {
+void buildACAutomaton(Node* root, vector<string>& keywords) {
     // 构建 Trie 树
     for (const string& word : keywords) {
         Node* curr = root;
@@ -25,6 +30,8 @@ void buildACAutomaton(Node* root, const vector<string>& keywords) {
             curr = curr->children[c];
         }
         curr->isEnd = true;
+        curr->minLen = min(curr->minLen,int(word.length()));
+        curr->data = word;
     }
 
     // 构建失败指针
@@ -50,50 +57,48 @@ void buildACAutomaton(Node* root, const vector<string>& keywords) {
             }
             
             child->fail = (failNode != nullptr) ? failNode->children[c] : root;
+            child->minLen = min(child->minLen,child->fail->minLen);
         }
     }
 }
 
-// 在文本中查找敏感词
-vector<string> findSensitiveWords(const string& text, Node* root) {
-    vector<string> sensitiveWords;
-    Node* curr = root;
-    for (char c : text) {
-        while (curr != nullptr && curr->children.find(c) == curr->children.end()) {
-            curr = curr->fail;
-        }
-        if (curr != nullptr) {
-            curr = curr->children[c];
-            if (curr->isEnd) {
-                sensitiveWords.push_back("Sensitive word found");
+class Solution {
+public:
+    int longestValidSubstring(string word, vector<string>& forbidden) {
+        Node* root = new Node();
+        buildACAutomaton(root,forbidden);
+        int left = 0;
+        int ans = 0;
+        int n = word.length();
+        Node* curr = root;
+        for(int i=0;i<n;i++){
+            while (curr != nullptr && curr->children.find(word[i]) == curr->children.end()) {
+                curr = curr->fail;
             }
-        } else {
-            curr = root;
+            if (curr != nullptr) {
+                curr = curr->children[word[i]];
+                if (curr->minLen != inf) {
+                    left = max(left,i+1-curr->minLen+1);
+                    ans = max(ans,i-left+1);
+                    curr = curr->fail;
+                } else {
+                    ans = max(ans,i - left + 1);
+                }
+            } else {
+                curr = root;
+                ans = max(ans,i - left + 1);
+            }
+
+            // std::cout << i << '\t' << left << '\t' << ans << std::endl;
         }
+        return ans;
     }
-    return sensitiveWords;
-}
+}; 
 
-int main() {
-    // 构建敏感词列表
-    vector<string> keywords = { "bad", "evil", "danger" };
-
-    // 构建 AC 自动机
-    Node* root = new Node();
-    buildACAutomaton(root, keywords);
-
-    // 检测敏感词
-    string text = "There is a baddanger guy in the neighborhood.";
-    vector<string> sensitiveWords = findSensitiveWords(text, root);
-
-    // 输出结果
-    if (sensitiveWords.empty()) {
-        cout << "No sensitive words found." << endl;
-    } else {
-        for (const string& word : sensitiveWords) {
-            cout << word << endl;
-        }
-    }
-
+int main(int argc, char const *argv[])
+{
+    string word = "aaaabaaacc";
+    vector<string> forbidden = {"bcca","aaa","aabaa","baaac"};
+    std::cout << Solution().longestValidSubstring(word,forbidden) << std::endl;
     return 0;
 }
