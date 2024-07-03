03b5316e2d0607c4364372168b7e585949e639c3
wxquare <wxquare@163.com>
2024-07-03 23:36:49 +0800

    add math

--- other/数学.cc
+++ other/数学.cc
@@ -50,10 +50,13 @@ int init = [&](){
 vector<int> getFactors(int x){
     vector<int> ans;
     for(int i=2;i*i<=x;i++){
-        if((x % i) == 0){
+        while((x % i) == 0){
             ans.push_back(i);
             ans.push_back(x / i);
         }
+    }
+    if(x > 1){
+        ans.push_back(x);
     }
     return ans;
 }
